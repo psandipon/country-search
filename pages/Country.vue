fc4079d1f24f2b35ac95b701898484173a5a095c
psandipon@gmail.com <psandipon@gmail.com>
2021-06-09 18:02:38 +0600

    country up

--- pages/Country.vue
+++ pages/Country.vue
@@ -1,14 +1,46 @@
 <template>
   <div>
-    <v-text-field
-      label="Search Country"
-      placeholder="Search Country"
-      outlined
-      v-model="search"
-      prepend-inner-icon="mdi-magnify"
-    ></v-text-field>
-    <v-btn @click="fetchCountry"> Search </v-btn>
-    <div v-if="load">{{ GET_ALL_COUNTRY }}</div>
+    <v-row class="ma-3">
+      <v-col cols="10">
+        <v-text-field
+          label="Search Country"
+          placeholder="Search Country"
+          outlined
+          v-model="search"
+          prepend-inner-icon="mdi-magnify"
+          dense
+        ></v-text-field>
+      </v-col>
+      <v-col cols="2" v-if="!ifPhone">
+        <v-btn outlined @click="fetchCountry" block>
+          Search
+        </v-btn>
+      </v-col>
+      <v-col cols="2" v-else>
+        <v-btn outlined small dark fab color="pink" @click="fetchCountry">
+          <v-icon> mdi-magnify </v-icon>
+        </v-btn>
+      </v-col>
+    </v-row>
+
+    <v-row>
+      <v-col
+        v-for="(item, i) in GET_ALL_COUNTRY"
+        :key="i"
+        cols="12"
+        lg="4"
+        md="6"
+        sm="12"
+        xs="12"
+      >
+        <div>
+          <v-card class="ma-3 pa-3">
+            <span class="title"> {{ item.name }} </span>
+            <v-img :src="item.flag"> </v-img>
+          </v-card>
+        </div>
+      </v-col>
+    </v-row>
   </div>
 </template>
 
@@ -18,14 +50,17 @@ export default {
   data() {
     return {
       search: "",
-      allCountries: [],
-      load: true
+      allCountries: []
     };
   },
 
   computed: {
-    ...mapGetters("module/country", ["GET_ALL_COUNTRY"])
+    ...mapGetters("module/country", ["GET_ALL_COUNTRY"]),
+    ifPhone() {
+      return this.$vuetify.breakpoint.mobile;
+    }
   },
+
   methods: {
     ...mapActions("module/country", ["FETCH_ALL_COUNTRY_DATA"]),
     fetchCountry() {
